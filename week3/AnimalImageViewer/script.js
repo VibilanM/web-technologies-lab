c9cfcecaca0993c995b53c05a0b1e3ab39c78a2e
Vibilan <vibilan2793@gmail.com>
2026-07-30 10:19:24 +0530

    animal image viewer js

--- week3/AnimalImageViewer/script.js
+++ week3/AnimalImageViewer/script.js
@@ -9,15 +9,12 @@ function showImage() {
     let animal = document.getElementById("animalName").value.toLowerCase();
 
     let img = document.getElementById("animalImage");
-    let msg = document.getElementById("message");
 
     if (animals[animal]) {
         img.src = "images/" + animals[animal];
-        msg.textContent = "";
     }
     else {
         img.src = "";
         img.alt = "Image Not Found.";
-        msg.textContent = "Image Not Found.";
     }
 }
