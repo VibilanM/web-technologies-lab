5068e731cfaa9c90b44db0578bc5ae2fbd6a1bd5
Vibilan <vibilan2793@gmail.com>
2026-08-27 09:38:05 +0530

    conference app footer.jsx

---
+++ week6/conference-app/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>
+          © 2026 International Conference on Emerging Technologies
+        </p>
+
+        <p>
+          All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
