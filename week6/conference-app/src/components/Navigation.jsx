e001e566b284ded3110c9c6d89a190f96ae0c25c
Vibilan <vibilan2793@gmail.com>
2026-08-27 09:34:41 +0530

    conference app navigation.jsx

---
+++ week6/conference-app/src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <div className="container">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#dates">Important Dates</a>
+        <a href="#speakers">Speakers</a>
+        <a href="#schedule">Schedule</a>
+        <a href="#registration">Registration</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
