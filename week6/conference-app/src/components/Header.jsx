f6ed4ae1e89429def6df23d8e306d4d5c834626a
Vibilan <vibilan2793@gmail.com>
2026-08-27 09:34:00 +0530

    conference app header.jsx

---
+++ week6/conference-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <h2>ICCIDS 2026</h2>
+        <p>International Conference on Emerging Technologies</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
